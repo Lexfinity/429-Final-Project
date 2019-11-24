3c3284d25df2db344839121f85c7a155380fbefd
Paul Attara <paul.attara@mail.mcgill.ca>
2019-11-24 16:56:44 -0500

    add error support for library of mutant generation

--- mutant-list-generator.py
+++ mutant-list-generator.py
@@ -6,21 +6,24 @@ newLines = []
 lineNumberCount = 1
 
 if len(sys.argv) != 2:
-    print("Invalid arguments: Please put SUT as second argument")
+    print "Invalid arguments: Please put SUT as second argument"
     sys.exit()
 
-print('Generating Mutant List for:', str(sys.argv[1]))
+print "Generating Mutant List for:", str(sys.argv[1])
 newLines.append("Library of Mutants\n\n")
 newLines.append("Line\t\tMutant\n")
+try:
+    with open(str(sys.argv[1])) as f:
+        lines = f.readlines()
 
-with open(str(sys.argv[1])) as f:
-    lines = f.readlines()
+        for line in lines:
+            if "/" in line or "+" in line or "-" in line or "=" in line or "*" in line:
+                newLines.append(str(lineNumberCount) +  "\t\t" + str(line))
+            lineNumberCount+=1
 
-    for line in lines:
-        if "/" in line or "+" in line or "-" in line or "=" in line or "*" in line:
-            newLines.append(str(lineNumberCount) +  "\t\t" + str(line))
-        lineNumberCount+=1
-
-    # lines = [l for l in lines if "/" in l]
-    with open("library-of-mutants.txt", "w") as f1:
-        f1.writelines(newLines)
+        # lines = [l for l in lines if "/" in l]
+        with open("library-of-mutants.txt", "w") as f1:
+            f1.writelines(newLines)
+except:
+    print "Error with file"
+    sys.exit()
