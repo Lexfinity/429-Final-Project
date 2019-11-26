46c5e40dcd3791b9b02fa2c70fb410c7a900a814
MassivaM <hamlimassy@hotmail.com>
2019-11-25 20:37:28 -0500

    parallel not done

---
+++ parallelsimulations.py
@@ -0,0 +1,47 @@
+import threading
+
+f=open("library-of-mutants.txt", "r")
+
+if f.mode == 'r':
+    contents = f.readlines()
+    lines = [content.replace('\n', '') for content in contents]
+    print(lines)
+
+f.close()
+
+f=open("mutants.txt", "r")
+
+if f.mode == 'r':
+    contents = f.readlines()
+    mLines = [content.replace('\n', '') for content in contents]
+    print(mLines)
+
+f.close()
+
+
+
+def threadingloop(l, m): 
+
+    expected = eval(l)
+    actual = eval(m)
+    if(len(l) == len(m)):
+        if(expected != actual):
+                print("mutant killed! \n""expression: %s" %l + "\t expected: %f\n" %expected + "expression: %s" %m + "\t actual: %f" %actual)
+
+
+
+a = 1
+b = 2
+
+for l in lines:
+    for m in mLines: 
+        t = threading.Thread(target=threadingloop, args=(l, m))
+        t2 = threading.Thread(target=threadingloop, args=(l, m))
+        t3 = threading.Thread(target=threadingloop, args=(l, m))
+   
+        t.start()
+        t2.start()
+        t3.start()
+
+
+        
